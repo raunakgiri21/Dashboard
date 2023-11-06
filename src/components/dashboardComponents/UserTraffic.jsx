73d3578fcd8754a99dbc975478c2a597eaeeb83a
Raunak <raunakgiri21@gmail.com>
2023-11-06 19:08:31 +0530

    Location

--- src/components/dashboardComponents/UserTraffic.jsx
+++ src/components/dashboardComponents/UserTraffic.jsx
@@ -1,8 +1,83 @@
 import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ReferenceLine,
+  ResponsiveContainer,
+  Cell,
+} from "recharts";
+
+const data = [
+  {
+    name: "Jan",
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Mar",
+    pv: -5800,
+    amt: 2290,
+  },
+  {
+    name: "Apr",
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "May",
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Jun",
+    pv: -3800,
+    amt: 2500,
+  },
+  {
+    name: "Jul",
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Aug",
+    pv: -9800,
+    amt: 2290,
+  },
+  {
+    name: "Sep",
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Oct",
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Nov",
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Nov",
+    pv: 2400,
+    amt: 2400,
+  },
+];
 
 export default function UserTraffic() {
   return (
-    <div className="flex flex-row w-full h-full min-h-[400px] bg-inherit border border-slate-800 rounded-xl shadow-shadowOne py-4 px-5">
+    <div className="flex flex-col w-full h-full min-h-[400px] bg-inherit border border-slate-800 rounded-xl shadow-shadowOne py-4 px-5">
       <div className="flex md:flex-row flex-col justify-between md:items-center h-12 w-full">
         <div>
           <p className="text-xl text-white font-semibold">User Traffic</p>
@@ -27,7 +102,45 @@ export default function UserTraffic() {
           </div>
         </div>
       </div>
-      <div></div>
+
+      <div>
+        <ResponsiveContainer
+          height={300}
+          width="100%"
+        >
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 30,
+              right: 10,
+              left: 0,
+              bottom: 0,
+            }}
+            barSize={30}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            {/* <Tooltip /> */}
+            <ReferenceLine
+              y={0}
+              stroke="#000"
+            />
+            <Bar
+              dataKey="pv"
+              fill="#8884d8"
+            >
+              {data.map((entry, index) => {
+                const color =
+                  entry.pv > 0 ? "rgb(59,130,246)" : "rgb(239,68,68)";
+                return <Cell fill={color} />;
+              })}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }

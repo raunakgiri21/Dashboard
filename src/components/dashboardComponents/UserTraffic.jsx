325777b3d3cc2513db663d31da42e61a07c823f1
Raunak <raunakgiri21@gmail.com>
2023-11-06 19:17:54 +0530

    User Traffic

--- src/components/dashboardComponents/UserTraffic.jsx
+++ src/components/dashboardComponents/UserTraffic.jsx
@@ -82,18 +82,20 @@ export default function UserTraffic() {
         <div>
           <p className="text-xl text-white font-semibold">User Traffic</p>
         </div>
-        <div className="flex md:gap-8 gap-4 md:mt-0 mt-2">
-          <div className="flex justify-center items-center md:gap-2 gap-1">
-            <div
-              className={`w-3 h-3 rounded-sm bg-blue-500 flex justify-center items-center`}
-            ></div>
-            <p className="text-slate-400 text-sm">User Sign Up</p>
-          </div>
-          <div className="flex justify-center items-center md:gap-2 gap-1">
-            <div
-              className={`w-3 h-3 rounded-sm bg-red-500 flex justify-center items-center`}
-            ></div>
-            <p className="text-slate-400 text-sm">User Subscribed</p>
+        <div className="flex md:gap-8 gap-4 md:mt-0 mt-2 justify-between">
+          <div className="flex md:gap-8 gap-4">
+            <div className="flex justify-center items-center md:gap-2 gap-1">
+              <div
+                className={`w-3 h-3 rounded-sm bg-blue-500 flex justify-center items-center`}
+              ></div>
+              <p className="text-slate-400 text-sm">User Sign Up</p>
+            </div>
+            <div className="flex justify-center items-center md:gap-2 gap-1">
+              <div
+                className={`w-3 h-3 rounded-sm bg-red-500 flex justify-center items-center`}
+              ></div>
+              <p className="text-slate-400 text-sm">User Subscribed</p>
+            </div>
           </div>
           <div>
             <select className=" cursor-pointer bg-inherit text-slate-500 border border-purple-900 rounded-[7px] px-[2px] py-[2px] md:w-28 w-16 text-sm">
@@ -113,14 +115,17 @@ export default function UserTraffic() {
             height={300}
             data={data}
             margin={{
-              top: 30,
+              top: 40,
               right: 10,
-              left: 0,
+              left: -10,
               bottom: 0,
             }}
             barSize={30}
           >
-            <CartesianGrid strokeDasharray="3 3" />
+            <CartesianGrid
+              strokeDasharray="3 3"
+              vertical={false}
+            />
             <XAxis dataKey="name" />
             <YAxis />
             {/* <Tooltip /> */}
